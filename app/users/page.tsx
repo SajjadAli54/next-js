78202cdc7f5ff2c34fba8de9ef83aba5e6e298bc
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2024-03-11 07:04:04 -0700

    Data Cache in fetch api

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,24 +1,25 @@
-import React, { use } from 'react'
+import React, { use } from "react";
 
-interface User{
-  id: number,
-  name: string
+interface User {
+  id: number;
+  name: string;
 }
 
 const UsersPage = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const response = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 10 },
+  });
   const users: User[] = await response.json();
   return (
     <div>
       <h1>Users</h1>
       <ul>
-        {
-          users.map(user => <li key={user.id}>{user.name}</li>)
-        }
-
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default UsersPage
+export default UsersPage;
