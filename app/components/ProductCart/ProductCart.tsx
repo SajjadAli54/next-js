4262dbd858a4931886745943b02b867ba21e0843
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2024-03-11 07:58:04 -0700

    daisy Ui

--- app/components/ProductCart/ProductCart.tsx
+++ app/components/ProductCart/ProductCart.tsx
@@ -3,7 +3,7 @@ import AddToCart from "../AddToCart";
 
 const ProductCart = () => {
   return (
-    <div className="p-4 my-5 bg-sky-400 text-white text-2x1 hover:bg-sky-600">
+    <div>
       <AddToCart />
     </div>
   );
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,6 +3,7 @@ import React, { use } from "react";
 interface User {
   id: number;
   name: string;
+  email: string;
 }
 
 const UsersPage = async () => {
@@ -14,11 +15,22 @@ const UsersPage = async () => {
     <div>
       <h1>Users</h1>
       <p>{new Date().toLocaleTimeString()}</p>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <td>Username</td>
+            <td>User Email</td>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
